33f155a3e4366c8e353291e62e8c6cb5d5fc92c5
kataras <kataras2006@hotmail.com>
2017-06-05 03:32:17 +0300

    https://github.com/massanchik/iris-boilerplate/pull/1

--- routes.go
+++ routes.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/kataras/iris"
 	// Import handlers package, change this to your own package.
-	"github.com/iris-contrib/iris-boilerplate/handlers"
+	"github.com/massanchik/iris-boilerplate/handlers"
 )
 
 //Setup routes
